4ec7d10a4f6308f0c62c1691163005db35800794
Rajitha Liyanage <rajitha1911@gmail.com>
2023-12-15 17:58:19 +0900

    add user model

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttergram/resources/storage_methods.dart';
+import 'package:fluttergram/models/user.dart' as models;
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -31,15 +32,19 @@ class AuthMethods {
             await StorageMethods().uploadImage('profilePics', file, false);
 
         //add user to firestore
-        await _firebaseFirestore.collection('users').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'profilePicUrl': profilePicUrl
-        });
+        models.User user = models.User(
+            username: username,
+            uid: cred.user!.uid,
+            email: email,
+            bio: bio,
+            followers: [],
+            following: [],
+            profilePicUrl: profilePicUrl);
+
+        await _firebaseFirestore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
         res = 'success';
       } else {
         return res;
@@ -58,7 +63,8 @@ class AuthMethods {
 
     try {
       if (email.isNotEmpty && password.isNotEmpty) {
-        await _auth.signInWithEmailAndPassword(email: email, password: password);
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
         res = 'success';
       } else {
         res = 'Please enter email & password';
